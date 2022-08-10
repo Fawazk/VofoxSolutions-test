b4c9c9223467276b9493b76bf167eda9f90a3eef
fawaz <fawazfkp2@gmail.com>
2022-08-10 12:25:22 +0530

    first commit

--- vofox/vofox/settings.py
+++ vofox/vofox/settings.py
@@ -65,8 +65,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'category.context_processors.menu_links',
-                'cart.context_processors.counter',
             ],
         },
     },
